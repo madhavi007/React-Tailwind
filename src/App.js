059427a038bb69b0ad00ddf9ed4d3eb5fc01fd94
Mjariwala07 <mjariwala@elitedigital.ca>
2021-10-14 23:23:03 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+
+  const [resultData, setresultData] = useState([]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(async () => {
+    try{
+      const response = await fetch("https://dog.ceo/api/breed/hound/images/random/10", {
+        method: "GET",
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setresultData(data.message)
+      }
+    }catch(error){
+      console.log(`error`, error)
+    } 
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid grid-cols-2 gap-x-5 gap-y-5 w-4/5">
+     {
+      resultData.map(function (index) {
+        return (
+          <div>
+            <img src={index} alt="" className="object-fill w-full h-full"/>
+          </div> 
+        )
+      })
+     }
     </div>
   );
 }
-
+ 
 export default App;
